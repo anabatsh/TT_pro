1f69dbd96378aff5ab91e41a5bf29de657517aab
AndreiChertkov <andre.chertkov@gmail.com>
2023-01-24 14:39:49 +0300

    Stable code for tests with analyt functions

--- calc.py
+++ calc.py
@@ -7,7 +7,7 @@ from time import perf_counter as tpc
 from protes import protes
 from problems.control import control_build
 from problems.control import control_solve_baseline
-from problems.func import func_build_function
+from problems.func import func_run
 from problems.qubo import qubo_build_function
 from problems.qubo import qubo_build_matrix
 from problems.qubo import qubo_solve_baseline
@@ -43,29 +43,21 @@ def calc_control(d=20, M=1.E+3, K=20, k=1, k_gd=50, r=5, lr=1.E-4):
     log(f'n opt ref >> {"".join([str(n) for n in n_opt_ref])}')
 
 
-def calc_func(d=10, n=20, M=1.E+4, K=10, k=1, k_gd=50, r=5, lr=1.E-4):
-    """Perform simple computations to test / check the method."""
+def calc_func(d=100, n=25, M=1.E+5, K=20, k=1, k_gd=50, r=5, lr=1.E-4):
+    """Perform computations for analytical multivariable functions."""
     log = Log(f'result/logs/func.txt')
-    time = tpc()
 
     txt = f'--> FUNC | '
     txt += f'd={d} n={n} M={M:-7.1e} K={K} k={k} k_gd={k_gd} r={r}'
     log(txt)
 
-    f = func_build_function(d, n, 'Schaffer')
+    def opt(f):
+        return protes(f, d, n, M, K, k, k_gd, r, lr, batch=True, log=False)
 
-    t = tpc()
-    n_opt = protes(f, d, n, M, K, k, k_gd, r, lr,
-        batch=True, log=True)
-    y_opt = f(n_opt)
-    t = tpc() - t
-
-    log(f'\n--------')
-    log(f'Result : {y_opt:-14.7e}')
-    log(f'Time   : {t:-14.3f}')
+    func_run(d, n, M, opt, log)
 
 
-def calc_qubo(d=10, M=1.E+4, K=20, k=1, k_gd=50, r=5, lr=1.E-4):
+def calc_qubo(d=250, M=1.E+5, K=20, k=1, k_gd=10, r=5, lr=1.E-4):
     """Solve the QUBO problem."""
     log = Log(f'result/logs/qubo.txt')
 
--- problems/func.py
+++ problems/func.py
@@ -1,13 +1,34 @@
 import teneva
+from ttopt import TTOpt
 
 
-FUNC_LIST = [
-    'Ackley', 'Alpine', 'Dixon', 'Exponential',
-    'Grienwank', 'Michalewicz', 'Qing', 'Rastrigin', 'Schaffer', 'Schwefel'
-]
+def func_run(d, n, M, opt, log):
+    for func in teneva.func_demo_all(d):
+        # Set the grid:
+        func.set_grid(n, kind='uni')
 
+        # OWN: Find min value for the original tensor by the proposed method:
+        n_opt_own = opt(func.get_f_ind)
+        y_opt_own =func.get_f_ind(n_opt_own)
 
-def func_build_function(d, n, name='Schaffer'):
-    func = teneva.func_demo_all(d, names=[name])[0]
-    func.set_grid(n, kind='uni')
-    return func.get_f_ind
+        # BS1: Find min value the original tensor by TTOpt:
+        tto = TTOpt(func.get_f_ind, d=d, n=n, evals=M, is_func=False)
+        tto.minimize()
+        n_opt_bs1 = tto.i_min
+        y_opt_bs1 = func.get_f_ind(n_opt_bs1)
+
+        # BS2: Find min value for TT-tensor by Optima-TT
+        # (we build the TT-approximation by the TT-CROSS method):
+        Y = teneva.tensor_rand(func.n, r=1)
+        Y = teneva.cross(func.get_f_ind, Y, e=1.E-16, m=M, dr_max=2)
+        Y = teneva.truncate(Y, e=1.E-16)
+        n_opt_bs2 = teneva.optima_tt(Y)[0]
+        y_opt_bs2 = func.get_f_ind(n_opt_bs2)
+
+        # Present the result:
+        text = ''
+        text += func.name + ' ' * max(0, 12-len(func.name)) +  ' | '
+        text += f'PROTES: {y_opt_own:-9.2e} | '
+        text += f'TTOpt: {y_opt_bs1:-9.2e} | '
+        text += f'Optima-TT: {y_opt_bs2:-9.2e} | '
+        log(text)
