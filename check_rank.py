d47f36fce9cf076c4abff252c4d07fe2bd6ca309
AndreiChertkov <andre.chertkov@gmail.com>
2023-02-03 17:10:45 +0300

    Some fixes

--- check_rank.py
+++ check_rank.py
@@ -9,64 +9,33 @@ from utils import Log
 from utils import folder_ensure
 
 
-def calc(M=int(1.E+2), r_list=[1, 3], reps=1):
+def calc(M=int(1.E+4), r_list=[1, 3, 5, 7, 9], reps=1):
     log = Log(f'result/logs_check_rank/result.txt')
 
-    d, f = build_knapsack()
+    tasks = {
+        'anten': {'d': 50, 'n': 2,
+            'f': build_knapsack()[1] },
+        'con-b': {'d': 50, 'n': 2,
+            'f': build_control(50)[0] },
+        'con-c': {'d': 50, 'n': 2,
+            'f': build_control(50, constr=True)[0], 'constr': True},
+    }
 
-    for r in r_list:
-        name = f'antenna-{d}D-{r}R'
-        text = name + ' ' * max(0, 15-len(name)) +  ' > '
-        y_opt_all = []
-        for rep in range(reps):
-            t = tpc()
-            n_opt, y_opt = protes(f, d, 2, M, r=r, log=True)
-            y_opt_all.append(y_opt)
-            t = tpc() - t
-            if rep == 0:
-                text += f't = {t:-9.2e} | '
-            text += f'v{rep} = {y_opt:-9.2e} | '
-        text += f'<v> = {np.mean(y_opt_all):-9.2e} | '
-        log(text)
-
-    d = 5
-    constr = False
-    f, args = build_control(d, constr=constr)
-
-    for r in r_list:
-        name = f'control-{d}D-{r}R'
-        text = name + ' ' * max(0, 15-len(name)) +  ' > '
-        y_opt_all = []
-        for rep in range(reps):
-            t = tpc()
-            n_opt, y_opt = protes(f, d, 2, M, r=r, constr=constr, log=True)
-            y_opt_all.append(y_opt)
-            t = tpc() - t
-            if rep == 0:
-                text += f't = {t:-9.2e} | '
-            text += f'v{rep} = {y_opt:-9.2e} | '
-        text += f'<v> = {np.mean(y_opt_all):-9.2e} | '
-        log(text)
-
-
-    d = 5
-    constr = True
-    f, args = build_control(d, constr=constr)
-
-    for r in r_list:
-        name = f'control-c-{d}D-{r}R'
-        text = name + ' ' * max(0, 15-len(name)) +  ' > '
-        y_opt_all = []
-        for rep in range(reps):
-            t = tpc()
-            n_opt, y_opt = protes(f, d, 2, M, r=r, constr=constr, log=True)
-            y_opt_all.append(y_opt)
-            t = tpc() - t
-            if rep == 0:
-                text += f't = {t:-9.2e} | '
-            text += f'v{rep} = {y_opt:-9.2e} | '
-        text += f'<v> = {np.mean(y_opt_all):-9.2e} | '
-        log(text)
+    for name_task, task in tasks.items():
+        for r in r_list:
+            text = f'{name_task}-{task["d"]}D-{r}R > '
+            y_opt_all = []
+            for rep in range(reps):
+                t = tpc()
+                n_opt, y_opt = protes(task['f'], task['d'], task['n'], M, r=r,
+                    constr=task.get('constr'), log=True)
+                y_opt_all.append(y_opt)
+                t = tpc() - t
+                if rep == 0:
+                    text += f't {t:-7.1e} | v '
+                text += f'{y_opt:-8.1e} '
+            text += f'| <v> {np.mean(y_opt_all):-9.2e}'
+            log(text)
 
 
 if __name__ == '__main__':
@@ -74,4 +43,5 @@ if __name__ == '__main__':
 
     folder_ensure('result')
     folder_ensure('result/logs_check_rank')
+
     calc()
